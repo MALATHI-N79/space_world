286bab52275b77519f87cf34e9a756e21084afb0
MALATHI-N79 <malathi3925@gmail.com>
2025-02-22 22:46:56 +0530

    components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-primary text-white text-center py-4">
+      <div>Copyright &copy; 2025 || MDK</div>
+    </div>
+  );
+};
+
+export default Footer;
